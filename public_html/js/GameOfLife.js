6684277c3fb129e82dca4043cd7e27d0483f6e5e
Jared Layne <jlayne@live.unc.edu>
2015-10-09 13:06:46 -0400

    createGameField() is working correctly.

--- public_html/js/GameOfLife.js
+++ public_html/js/GameOfLife.js
@@ -1,15 +1,31 @@
 
 $(document).ready(function () {
     var size = 10;
-    var $rowDiv = $("<div></div>", {class: "rowCell"});
-    var $columnDiv = $("<div></div>", {class: "columnCell"});
-    for (var i = 0; i < size; i++) {
-        var rowArray = [];
-        for (var j = 0; j < size; j++) {
-            $columnDiv.append($rowDiv.clone().append(j));
-        }
-        $("#gameField").append($columnDiv.clone());
-    }
+    $("#cellCountSubmitButton").on("click", function () {
+        getGameFieldSize();
+    });
+
+    createGameField(size);
+
 
 
 });
+
+function createGameField(size) {
+    var $rowDiv = $("<div></div>", {class: "rowCell"});
+    var $columnDiv = $("<div></div>", {class: "columnCell"});
+    for (var i = 0; i < size; i++) {
+        $("#gameField").append($rowDiv.clone().attr("id", "rowCell" + i));
+    }
+    // columnCell numbering index
+    var i = 0;
+    $(".rowCell").each(function () {
+        $(".rowCell").append($columnDiv.attr("id", "columnCell" + i));
+        i++;
+    });
+}
+
+function getGameFieldSize() {
+    var numberOfCells = $("#cellCount").val();
+    return numberOfCells;
+}
