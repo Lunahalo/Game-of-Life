43a223769c9685c352b84a8fc053fac24837f9a2
Jared Layne <jlayne@live.unc.edu>
2015-10-17 13:10:17 -0400

    Can draw grid correctly.

--- public_html/js/GameOfLife.js
+++ public_html/js/GameOfLife.js
@@ -1,5 +1,18 @@
 
 $(document).ready(function () {
+    /*var c = document.getElementById("gameCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(10,0);
+    ctx.lineTo(10,100);
+    ctx.stroke();*/
+    ctx = $('#gameCanvas').get(0).getContext('2d');
+    gameCanvas = ctx.canvas;
+    gameCanvas.width = $('#gameFieldDiv').width();
+    gameCanvas.height = gameCanvas.width;
+    
+    createGrid(10);
+    //alert(gameCanvas.width);
+    //drawLine(10 , gameCanvas.hight);
     //createGameField(40);
     //$("#cellCountSubmitButton").click(function (event) {
     //    return getGameFieldSize();
@@ -10,6 +23,28 @@ $(document).ready(function () {
     //createGameField(size);
     //
 });
+
+
+function createGrid(numOfCellOnSize) {
+    var offset = 0.5;
+    //Figure out what the largest cell size we can use without going over the game
+    var cellSize = Math.floor(gameCanvas.width/numOfCellOnSize);
+    //The canvas size should now be an even multiple of this.
+    gameCanvas.width = numOfCellOnSize * cellSize;
+    gameCanvas.height = gameCanvas.width;
+    var totalWidth = numOfCellOnSize * cellSize +offset;
+    $('#gameFieldDiv').width(totalWidth);
+    for(var x = cellSize; x < totalWidth; x += cellSize ){
+        ctx.moveTo(x+offset, 0);
+        ctx.lineTo(x+offset, gameCanvas.height);
+    }
+    for(var y = cellSize; y < totalWidth; y += cellSize ){
+        ctx.moveTo(0, y+ offset);
+        ctx.lineTo(gameCanvas.width, y+ offset);
+    }
+    ctx.stroke();
+}
+
 
 function createGameField(size) {
     var $rowDiv = $("<div></div>", {class: "rowCell"});
