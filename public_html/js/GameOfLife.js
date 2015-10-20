3d403624d507671b03f39fb52946ce405e4ecd84
Jared Layne <jlayne@live.unc.edu>
2015-10-20 18:29:32 -0400

    Removed getGameFieldSize and createGameField because these used dom manipulation.

--- public_html/js/GameOfLife.js
+++ public_html/js/GameOfLife.js
@@ -343,20 +343,3 @@ function colorCellAt(xIndex, yIndex, color) {
     ctx.fillStyle = color;
     ctx.fillRect(xpos, ypos, sizeOfCell - offset, sizeOfCell - offset);
 }
-function createGameField(size) {
-    var $rowDiv = $("<div></div>", {class: "rowCell"});
-    var $columnDiv = $("<div></div>", {class: "columnCell"});
-    for (var i = 0; i < size; i++) {
-        $("#gameField").append($rowDiv.clone().attr("id", "rowCell" + i));
-    }
-    $(".rowCell").each(function (index) {
-        for (var j = 0; j < size; j++) {
-            $(this).append($columnDiv.clone().attr("id", "columnCell" + j));
-        }
-    });
-}
-
-function getGameFieldSize() {
-    var numberOfCells = $("#cellCount").val();
-    return numberOfCells;
-}
